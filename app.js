cc6cad6586d2a08cbcdc650f8cf2a90f535ae690
soso <165726652+sosoif@users.noreply.github.com>
2024-04-05 15:00:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+function getProjects(){
+    const urlGitHub = 'https://api.github.com/users/sosoif/repos'
+    var loadingElement = document.getElementById('loading')
+
+    fetch(urlGitHub,  {
+        method: 'GET'
+    })
+
+        .then((Response) => Response.json())
+        .then((Response) =>{
+            loadingElement.style.display ='none'
+            showProjects(Response)
+        })
+        .catch((e) =>{
+            console.log(e)
+        })
+}
+
+function showProjects(data){
+    var listElement =document.getElementById('my-projects-list')
+
+    for(let i =0; i < data.length; i++)
+    {
+        let a = document.createElement("a")
+        a.href = data[i]['clone_url']
+        a.target ='_blank'
+        a.title = data[i]['description']
+        let linkText = document.createTextNode(data[i]['name'])
+        a.appendChild(linkText)
+        listElement.appendChild(a)
+    }
+
+}
+
+getProjects()
